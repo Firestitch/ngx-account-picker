6f221bc805b75470be2fdaaa9ef16dd318be056f
Raymond Gigliotti <ray@firestitch.com>
2024-10-03 05:48:18 -0400

    cleaned up A12 issues

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
 
-import { FsExampleModule } from '@firestitch/example';
 import { FsBadgeModule } from '@firestitch/badge';
+import { FsExampleModule } from '@firestitch/example';
 
 import { FsAccountPickerModule } from '@firestitch/account-picker';
 import { FsChipModule } from '@firestitch/chip';
 
-import { AppMaterialModule } from './material.module';
-import {
-ExampleComponent,
-ExamplesComponent } from './components';
 import { AppComponent } from './app.component';
-import { ToastrModule } from 'ngx-toastr';
-
+import {
+  ExampleComponent,
+  ExamplesComponent
+} from './components';
+import { AppMaterialModule } from './material.module';
 
 const routes: Routes = [
   { path: '', component: ExamplesComponent },
@@ -28,24 +26,18 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     FsBadgeModule,
-    FlexModule,
     FsChipModule,
     FsAccountPickerModule,
     BrowserAnimationsModule,
     AppMaterialModule,
     FormsModule,
     FsExampleModule.forRoot(),
-    ToastrModule.forRoot({ preventDuplicates: true }),
     RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
-  ],
-  entryComponents: [
   ],
   declarations: [
     AppComponent,
     ExamplesComponent,
     ExampleComponent
-  ],
-  providers: [
   ],
 })
 export class PlaygroundModule {
